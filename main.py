322113ecdaed9b76f9b6cdadb325c120778832b3
DartSams <dartagnansams1@icloud.com>
2021-04-05 21:20:58 -0400

    game file

--- main.py
+++ main.py
@@ -1,63 +1,3 @@
-# import pygame as pg
-
-
-# def main():
-#     screen = pg.display.set_mode((640, 480))
-#     font = pg.font.Font(None, 32)
-#     clock = pg.time.Clock()
-#     input_box = pg.Rect(100, 100, 140, 32)
-#     color_inactive = pg.Color('lightskyblue3')
-#     color_active = pg.Color('dodgerblue2')
-#     color = color_inactive
-#     active = False
-#     text = ''
-#     done = False
-
-#     while not done:
-#         for event in pg.event.get():
-#             if event.type == pg.QUIT:
-#                 done = True
-#             if event.type == pg.MOUSEBUTTONDOWN:
-#                 # If the user clicked on the input_box rect.
-#                 if input_box.collidepoint(event.pos):
-#                     # Toggle the active variable.
-#                     active = not active
-#                 else:
-#                     active = False
-#                 # Change the current color of the input box.
-#                 # color = color_active if active else color_inactive
-#             if event.type == pg.KEYDOWN:
-#                 if active:
-#                     if event.key == pg.K_RETURN:
-#                         print(text)
-#                         text = ''
-#                     elif event.key == pg.K_BACKSPACE:
-#                         text = text[:-1]
-#                     else:
-#                         text += event.unicode
-
-#         screen.fill((30, 30, 30))
-#         # Render the current text.
-#         txt_surface = font.render(text, True, color)
-#         # Resize the box if the text is too long.
-#         width = max(200, txt_surface.get_width()+10)
-#         input_box.w = width
-#         # Blit the text.
-#         screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
-#         # Blit the input_box rect.
-#         pg.draw.rect(screen, color, input_box, 2)
-
-#         pg.display.flip()
-#         clock.tick(30)
-
-
-# if __name__ == '__main__':
-#     pg.init()
-#     main()
-#     pg.quit()
-
-
-
 import pygame
 import sys
 from datetime import datetime
@@ -96,7 +36,6 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos=pygame.mouse.get_pos()
             if start_screen.collidepoint(pos):
-                # print('next')
                 run=False
                 screen.fill((255,255,255))
                 game_screen=pygame.transform.scale(pygame.image.load(r"PyGame\speed typing\assets\background.png"),(width,height-100))
@@ -104,55 +43,31 @@ while True:
 
 
 
-            if type_area.collidepoint(pos):
-                active=True
-                print(active)
-
-            else:
-                active=False
-                print(active)
+        if event.type==pygame.KEYDOWN:
+            if len(sentence)==1:
+                start_time = time.time()
+                print('starting now')
 
 
-        # rn set to False to test as soon as it opens
-        if active==True:
-            # game_screen=pygame.transform.scale(pygame.image.load(r"PyGame\speed typing\assets\background.png"),(width,height-100))
-            # screen.blit(game_screen,(0,0))
+            sentence+=event.unicode
+            
 
+            if event.key==13:
+                print(sentence)
+                sentence=''
 
-            if event.type==pygame.KEYDOWN:
-                if len(sentence)==1:
-                    start_time = time.time()
+            if event.key==8:
+                sentence=sentence[:-1]
+    
 
-
-                sentence+=event.unicode
-                
-
-                if event.key==13:
-                    print(sentence)
-                    sentence=''
-
-                if event.key==8:
-                    sentence=sentence[:-1]
-        
-                ##hotkey for backspace
-                # if event.key==8:
-                #     sentence = sentence[:-1]
-
-                # ##hotkey for escape
-                # elif event.key==27:
-                #     now=datetime.now().second
-
-
-        # print(event)
     keys=pygame.key.get_pressed()
-    # screen.fill((50,70,255))
     if run==False:
         screen.fill((255,255,255))
         game_screen=pygame.transform.scale(pygame.image.load(r"PyGame\speed typing\assets\background.png"),(width,height-100))
         screen.blit(game_screen,(0,20))
 
     test_text_sentence=text.render(f"{test_sentence}",1,(0,0,0))
-    screen.blit(test_text_sentence,(0,5))
+    screen.blit(test_text_sentence,(5,5))
 
 
     type_area=pygame.draw.rect(screen,(0,0,0),(0,height-100,width,height+100))
@@ -165,6 +80,7 @@ while True:
         end_time = time.time()
         print(f"you took {round(end_time-start_time,5)} seconds to finish")
         test_sentence=''
+
 
 
         if len(lst)==0:
@@ -180,7 +96,6 @@ while True:
         start_screen=screen.blit(start_bg,(0,0))
         start_game=text.render(f"Click any where to begin" ,1,(255,255,255))
         screen.blit(start_game,(width//2-180,height-110))
-        # screen.fill((255,255,255))
 
     pygame.display.flip()
     FPS.tick(120)
